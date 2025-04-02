6de66fc68d2f83cf0fb8a20d22c5773b316a1a0f
nagstn <nagstn@gmail.com>
2025-04-02 13:03:09 +0530

    Superscript validation

--- src/test/java/org/example/CadrePagesSuperScriptValidation.java
+++ src/test/java/org/example/CadrePagesSuperScriptValidation.java
@@ -42,8 +42,8 @@ public class CadrePagesSuperScriptValidation {
     }
 
     @When("I navigate to {string}")
-    public void iNavigateTo(String url) {
-        driver.get(url);
+    public void iNavigateTo(String CurrentUrl) {
+        driver.get(CurrentUrl);
         pageTitle = driver.getTitle();
     }
 
